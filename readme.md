193e026610cb68036a6ac2f559ab300ca9ff99d5
Your Name <you@example.com>
2022-10-11 11:53:23 +0200

    Terminamos pantalla inicio, pendiente de alinear con base de datos

---
+++ readme.md
@@ -0,0 +1,4 @@
+|Leer paso por paso para ejecutar este programa sin problemas|
+
+<Instalar las dependencias Flask y Pyhton Dotenv>
+<En el fichero create.sql está la estructura de el archivo sql>
--- registro_cripto/routes.py
+++ registro_cripto/routes.py
@@ -1,8 +1,17 @@
 from registro_cripto import app
-
+from flask import render_template
 
 
 @app.route("/")
 def index():
-    return "Servidor Funcionando!!!"
+    return render_template("index.html", pageTitle="Movimientos")
+
+@app.route("/purchase")
+def purchase():
+    return render_template("purchase.html", pageTitle="Compra")
+
+@app.route("/status")
+def status():
+    return "En construcción"
+   
 
